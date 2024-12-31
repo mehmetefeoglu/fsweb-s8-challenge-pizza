9940c5ecc8de4b3330ef4c394fda7a848da2f741
mehmetefeoglu <mehmet.efeogludev@gmail.com>
2024-12-31 12:47:42 +0300

    app.jsx header, home,footer, order ve succes  birimleri
    app.jsx header, home,footer, order ve succes  birimlerinin jsx ve css dosyaları oluşturulup app.jsx e import edildi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import workintech from '/workintech.svg'
 import './App.css'
+import Header from './assets/components/header/header'
+import Home from './assets/components/home/home'
+import Order from './assets/components/order/order'
+import Success from './assets/components/success/success'
+import Footer from './assets/components/footer/footer'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
-          <img src={workintech} className="logo" alt="Workintech logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Workintech + 🍕</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Absolute Acı Pizza sayısı {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Workintech or Pizza logos to learn more
-      </p>
+    <Header/>
+    <Home></Home>
+    <Order></Order>
+    <Success></Success>
+    <Footer></Footer>
     </>
   )
 }
---
+++ src/assets/components/header/header.jsx
@@ -0,0 +1,5 @@
+export default function Header (){
+    return <div className="header">
+        <p>Teknolojik Yemekler</p>
+    </div>
+}
